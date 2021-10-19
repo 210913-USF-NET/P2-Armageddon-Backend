9f61a492ac2893f4c674457d593cfe2d918d75ac
Carlos A Martinez <Linguz2@gmail.com>
2021-10-19 16:48:35 -0400

    Database started, almost.

---
+++ DL/BattleshipDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DL
+{
+    public class BattleshipDBContext : DbContext
+    {
+        public BattleshipDBContext() : base() { }
+        public BattleshipDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ChatHistory> ChatHistory { get; set; }
+        public DbSet<Friends> Friends { get; set; }
+        public DbSet<Layout> Layout { get; set; }
+        public DbSet<Match> Match { get; set; }
+        public DbSet<Turn> Turn { get; set; }
+        public DbSet<User> User { get; set; }
+
+
+    }
+}
