b0658ba4fe8e2876920787f56d8fc80eaa55af0f
Tristan Postley <tpostley@gmail.com>
2021-10-19 13:36:48 -0700

    Finish Models

---
+++ Models/Friends.cs
@@ -0,0 +1,10 @@
+namespace Models
+{
+    public class Friends
+    {
+        Friends() {}
+        public int Id {get; set;}
+        public int user1Id {get; set;}
+        public int user2Id {get; set;}
+    }
+}
