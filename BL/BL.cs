c09bf79a98a9e771462f15d09e09752153947605
Tristan Postley <tpostley@gmail.com>
2021-10-19 14:17:02 -0700

    Migration

---
+++ BL/BL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DL;
+using Models;
+
+namespace BattleshipBL
+{
+    public class BL : IBL
+    {
+        private IRepo _repo;
+
+        public BL(IRepo repo)
+        {
+            _repo = repo;
+        }
+    }
+}
---
+++ DL/DBRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DL
+{
+    public class DBRepo : IRepo
+    {
+        private BattleshipDBContext _context;
+        public DBRepo(BattleshipDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
