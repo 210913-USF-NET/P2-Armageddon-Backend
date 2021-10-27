1668e896812acbd2b0341519d915523d48b19ef1
haoxinwong <2506891138@qq.com>
2021-10-27 09:48:22 -0400

    update

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,7 +45,7 @@ namespace WebAPI
                 builder =>
                 {
                     builder.AllowAnyMethod().AllowAnyHeader()
-                    .WithOrigins("localhost:4200/")
+                    .WithOrigins("http://localhost:4200/")
                     .AllowCredentials();
                 }));
         }
