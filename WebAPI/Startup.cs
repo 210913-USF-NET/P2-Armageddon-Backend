d6a463bfbf48d6a32989a51cae4a82ebb3d225d5
haoxinwong <2506891138@qq.com>
2021-10-27 08:55:22 -0400

    updatetest

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -40,6 +40,14 @@ namespace WebAPI
             options.UseNpgsql(Configuration.GetConnectionString("BattleshipDB")));
             services.AddScoped<IRepo, DBRepo>();
             services.AddScoped<IBL, BL>();
+            services.AddSignalR();
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy",
+                builder =>
+                {
+                    builder.AllowAnyMethod().AllowAnyHeader()
+                    .WithOrigins("http://localhost:4200/")
+                    .AllowCredentials();
+                }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +65,7 @@ namespace WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
